9cd31c4e3d49d331ca73c4f58cb69c17f2fe4af5
Vogrič <53317134+Vogric@users.noreply.github.com>
2020-07-28 20:49:55 -0300

    .

---
+++ src/components/Formulario.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+
+const Formulario = () => {
+  const [state, setState] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  const actualizarState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { mascota, propietario, fecha, hora, sintomas } = state;
+
+  const submitCita = (e) => {
+    e.preventDefault();
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      setError(true);
+      return;
+    }
+  };
+
+  const [error, setError] = useState(false);
+
+  return (
+    <>
+      <h2>Crear cita</h2>
+      <form onSubmit={submitCita}>
+        <label>Nombre mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+        <label>Nombre dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre dueño de la mascota"
+          onChange={actualizarState}
+          value={propietario}
+        />
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+        <label>Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+        <label>Sintomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+        <button tyoe="submit" className="u-full-width button-primary">
+          Agregar cita
+        </button>
+        {error ? (
+          <p className="alerta-error">Todos los campos son obligatorios</p>
+        ) : null}
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
