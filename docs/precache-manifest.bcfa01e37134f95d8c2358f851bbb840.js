2ccc2d67afb58e316c8f3f8859c9267881e092fc
Vogrič <53317134+Vogric@users.noreply.github.com>
2020-07-30 10:00:25 -0300

    .

---
+++ docs/precache-manifest.bcfa01e37134f95d8c2358f851bbb840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7af4b8e9ebb1fa3b2aa666b5a58c9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adcbc6164bda06d54709",
+    "url": "/static/css/main.e8e0c172.chunk.css"
+  },
+  {
+    "revision": "02c528cb543c24f923db",
+    "url": "/static/js/2.cb745f93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb745f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adcbc6164bda06d54709",
+    "url": "/static/js/main.d937e76c.chunk.js"
+  },
+  {
+    "revision": "9078284b3c5a7cc4441c",
+    "url": "/static/js/runtime-main.3c7fdb96.js"
+  }
+]);
